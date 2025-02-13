dd89a36da7cbf66011dfeb2c8ad1b67b97188b89
p4ddle <dewaputuadhitya@gmail.com>
2025-02-13 20:51:41 +0700

    UserService

--- app/Services/Impl/UserServiceImpl.php
+++ app/Services/Impl/UserServiceImpl.php
@@ -3,7 +3,13 @@
 namespace App\Services\Impl;
 
 use App\Services\UserService;
+use Illuminate\Support\Facades\Auth;
 
 class UserServiceImpl implements UserService {
-    
+    public function login(string $email, string $password){
+        return Auth::attempt([
+            "email" => $email,
+            "password" => $password
+        ]);
+    }
 }
