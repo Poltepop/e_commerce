115fb8ebe3562513b66b8af7a5e823cd024c4ecb
p4ddle <dewaputuadhitya@gmail.com>
2025-02-13 22:35:25 +0700

    User Controller

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,15 @@
 <?php
 
+use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+
+Route::controller(UserController::class)->group(function(){
+    route::get('/login', 'login');  
+    route::post('/login', 'doLogin');  
+    route::post('/logout', 'doLogout');  
 });
