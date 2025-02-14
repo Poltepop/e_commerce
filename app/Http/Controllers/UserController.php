23e2c19758f1923bc02628510924ba07972c2d48
p4ddle <dewaputuadhitya@gmail.com>
2025-02-14 09:35:03 +0700

    Logic Action

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -2,11 +2,21 @@
 
 namespace App\Http\Controllers;
 
+use App\Services\UserService;
+use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
 use Illuminate\Http\Response;
+use Illuminate\Support\Facades\Redirect;
 
 class UserController extends Controller
 {
+    private UserService $userService;
+
+    public function __construct(UserService $userService)
+    {
+        $this->userService = $userService;
+    }
+
     public function login(): Response
     {
         return Response()->view('auth.login',[
@@ -14,8 +24,28 @@ class UserController extends Controller
         ]);
     }
 
-    public function doLogin(){
+    public function doLogin(Request $request): Response | RedirectResponse
+    {
+        $email = $request->input('email');
+        $password = $request->input('password');
 
+        //Validate Input
+        if(empty($email) || empty($password)){
+            return response()->view('auth.login',[
+                'title' => 'Login',
+                'error' => 'Email Or Password Is Required',
+            ]);
+        } //If Email Or Password Is Empty
+
+        if($this->userService->login($email, $password)){
+            $request->session()->put('email', $email);
+            return redirect('/');
+        }// If Email Or Password Is Correct
+
+        return response()->view('auth.login',[
+            'title' => 'Login',
+            'error' => 'Email Or Pasword Is Wrong'
+        ]);// If Email Or Pasword Is Wrong
     }
 
     public function doLogout(){
