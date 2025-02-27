30f6814345438b994d65ecff515b3002fbc01418
p4ddle <dewaputuadhitya@gmail.com>
2025-02-27 21:39:22 +0700

    Update Form Create

--- resources/views/components/form-input.blade.php
+++ resources/views/components/form-input.blade.php
@@ -9,9 +9,9 @@
           </div>
 
           <div class="flex pt-4 mb-5 items-center">
-            <h1 class="text-4xl">Create</h1>
+            <h1 class="text-4xl font-bold">Create</h1>
             <div class="divider divider-horizontal"></div>
-            <p class="font-light text-sm">Tambah produk kamu di sini</p>
+            <p class="font-light text-sm text-gray-50">Add Your Product here</p>
           </div>
 
           @error('name')
@@ -38,14 +38,14 @@
 
           <form action="{{ route('save.product') }}" method="post">  
             @csrf
-            <div class="flex flex-col justify-center">
+            {{-- <div class="flex flex-col justify-center">
                 <select class="select select-bordered w-full mb-2" name="status">
                     <option value="active">Active</option>
                     <option value="inactive">Inactive</option>
                   </select>
                 <div class="flex flex-row justify-between mb-2">
                     <input name="name" type="text" placeholder="Name" class="input input-bordered w-full mr-1"/>
-                    <input name="slug" type="text" placeholder="Slug" class="input input-bordered w-full ml-1"/>
+                    <input name="slug" type="text" placeholder="Slug" class="input input-bordered w-full ml-1" disabled/>
                 </div>
                 <input name="price" type="text" placeholder="price" class="input input-bordered w-full mb-2"/>
                 <input name="weight" type="text" placeholder="Weight" class="input input-bordered w-full mb-2"/>
@@ -53,11 +53,124 @@
                     <textarea name="short_description" class="textarea textarea-bordered w-full mr-1" placeholder="Short Description"></textarea>
                     <textarea name="description" class="textarea textarea-bordered w-full ml-1" placeholder="Description"></textarea>
                 </div>
-            </div>
+            </div> --}}
 
-            <div class="mt-8">
+            {{-- <div class="mt-8">
                 <p class="text-xs font-thin mb-1">*Lorem ipsum dolor sit amet, consectetur adipisicing elit. Consectetur architecto quod non..</p>
                 <button class="btn btn-outline btn-accent w-60" type="submit">Save</button>
+            </div> --}}
+
+            <div class="flex">
+              {{-- right --}}
+              <div class="rounded-box flex-col flex-grow place-items-center mr-4 w-1/3">
+
+                {{-- Text Input --}}
+                  <div class="card rounded-box bg-base-100 h-auto w-full p-5">
+                    <div class="flex flex-row mb-3">
+                      <label class="form-control w-full max-w-lg mr-3">
+                        <div class="label">
+                          <span class="label-text text-gray-50">What is your name product?</span>
+                        </div>
+                        <input type="text" name="name" placeholder="Type here" class="input input-bordered w-full max-  w-lg" />
+                      </label>
+                      <label class="form-control w-full max-w-lg">
+                        <div class="label">
+                          <span class="label-text text-gray-50">Slug</span>
+                        </div>
+                        <input type="text" name="slug" placeholder="Type here" disabled class="input input-bordered w-full max-w-lg" />
+                      </label>
+                    </div>
+                    <label class="form-control">
+                      <div class="label">
+                        <span class="label-text text-gray-50">Your Description</span>
+                      </div>
+                      <textarea name="description" class="textarea textarea-bordered h-24" placeholder="Description"></textarea>
+                    </label>
+                    <label class="form-control mt-3">
+                      <div class="label">
+                        <span class="label-text text-gray-50">Short Description</span>
+                      </div>
+                      <textarea name="short_description" class="textarea textarea-bordered h-24" placeholder="Description"></textarea>
+                    </label>
+                  </div>
+                  {{-- File Input --}}
+                  <div class="card rounded-box bg-base-100 h-auto w-full mt-5">
+                    <div class="border-b-2 w-full">
+                      <h1 class="font-bold rounded-lg p-4">Image</h1>
+                    </div>
+                    <div class="p-5">
+                      <input type="file" class="file-input file-input-bordered file-input-lg w-full" />
+                    </div>
+                  </div>
+                  
+                  {{-- Pricing--}}
+                  <div class="card rounded-box bg-base-100 h-auto w-full mt-5">
+                    <div class="border-b-2 w-full">
+                      <h1 class="font-bold rounded-lg p-4">Pricing</h1>
+                    </div>
+                    <div class="p-5">
+                      <div class="flex flex-row">
+                        <label class="form-control w-full max-w-lg mr-3">
+                          <div class="label">
+                            <span class="label-text text-gray-50">Price</span>
+                          </div>
+                          <input type="text" name="price" placeholder="Price" class="input input-bordered w-full max-w-lg" />
+                        </label>
+                        <label class="form-control w-full max-w-lg">
+                          <div class="label">
+                            <span class="label-text text-gray-50">Weight</span>
+                          </div>
+                          <input type="text" name="weight" placeholder="Weight" class="input input-bordered w-full max-w-lg" />
+                        </label>
+                      </div>
+                    </div>
+                  </div>
+
+                  <div class="flex-row mt-5">
+                    <button class="btn btn-sm btn-outline btn-success">Create</button>
+                    <a href="/posts" class="btn btn-sm btn-outline btn-error">Cencel</a>
+                  </div>
+              </div>
+
+              {{-- left --}}
+              <div class="rounded-box flex-col flex-grow place-items-center max-w-xs">
+                <div class="card rounded-box bg-base-100 h-auto w-full">
+                  <div class="border-b-2 w-full">
+                    <h1 class="font-bold rounded-lg p-4">Status</h1>
+                  </div>
+                  <div class="p-5">
+                    <div class="form-control w-auto">
+                      <label class="label cursor-pointer">
+                        <span class="label-text">Active</span>
+                        <input type="checkbox" class="toggle toggle-accent" checked="checked" />
+                      </label>
+                      <p class="font-thin text-xs">This product will be hidden from all sales channels.</p>
+                    </div>
+                  </div>
+                </div>
+
+                <div class="rounded-box flex-col flex-grow place-items-center max-w-xs mt-5">
+                  <div class="card rounded-box bg-base-100 h-auto w-full">
+                    <div class="border-b-2 w-full">
+                      <h1 class="font-bold rounded-lg p-4">Associations </h1>
+                    </div>
+                    <div class="p-5">
+                      <label class="form-control w-full max-w-xs">
+                        <div class="label">
+                          <span class="label-text text-gray-50 text-xs">Category</span>
+                        </div>
+                        <select class="select select-bordered select-sm w-full max-w-xs">
+                          <option disabled selected>Small</option>
+                          <option>Small Apple</option>
+                          <option>Small Orange</option>
+                          <option>Small Tomato</option>
+                        </select>
+                      </label>
+                    </div>
+                  </div>
+                </div>
+              </div>
+
             </div>
           </form>
     </x-content>
