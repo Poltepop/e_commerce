8217abb8669fe5dc84cc89e1c01ff0edf39b3063
p4ddle <dewaputuadhitya@gmail.com>
2025-02-28 15:16:39 +0700

    provider

--- bootstrap/providers.php
+++ bootstrap/providers.php
@@ -2,6 +2,7 @@
 
 return [
     App\Providers\AppServiceProvider::class,
+    App\Providers\CategoryServiceProvider::class,
     App\Providers\ProductServiceProvider::class,
     App\Providers\UserServiceProvider::class,
 ];
