26eda47b1012a9c08c3603d95c7568edf26e605c
p4ddle <dewaputuadhitya@gmail.com>
2025-03-01 19:40:28 +0700

    Add Field And Logic Auto Generate Slug

---
+++ app/Models/Category.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Str;
+
+class Category extends Model
+{
+    protected $fillable = [
+        'name',
+        'slug',
+    ];
+
+    protected static function boot()
+    {
+        parent::boot();
+
+        static::creating(function ($category) {
+            $category->slug = static::generateUniqueSlug($category->name);
+        });
+    }
+
+    public static function generateUniqueSlug($name)
+    {
+        $slug = Str::slug($name);
+        $originalSlug = $slug;
+        $count = 1;
+
+        while (self::where('slug', $slug)->exists()) {
+            $slug = $originalSlug . '-' . $count;
+            $count++;
+        }
+
+        return $slug;
+    }
+}
