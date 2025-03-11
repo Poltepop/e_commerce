f21b5af24a4802afc9eb56d463a0b5ae26f86f54
p4ddle <dewaputuadhitya@gmail.com>
2025-03-11 11:00:29 +0700

    Add filter and check all

--- resources/views/admin/products.blade.php
+++ resources/views/admin/products.blade.php
@@ -54,16 +54,31 @@
 
     <div class="overflow-x-auto">
       <div class="card bg-base-100 w-full h-auto">
+        <form action="{{ route('add.wishlist') }}" method="POST">
         <div class="border-b-2 border-gray-700 p-5">
+          <div class="dropdown dropdown-hover">
+            <div tabindex="0" role="button" class="btn btn-square btn-outline btn-accent m-1"><svg class="w-6 h-6 text-gray-800 dark:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24">
+              <path stroke="currentColor" stroke-linecap="round" stroke-width="2" d="M18.796 4H5.204a1 1 0 0 0-.753 1.659l5.302 6.058a1 1 0 0 1 .247.659v4.874a.5.5 0 0 0 .2.4l3 2.25a.5.5 0 0 0 .8-.4v-7.124a1 1 0 0 1 .247-.659l5.302-6.059c.566-.646.106-1.658-.753-1.658Z"/>
+            </svg>
+            </div>
+            <ul tabindex="0" class="dropdown-content menu bg-base-100 rounded-box z-[1] w-52 p-2 shadow">
+              <li><button>wishlist</button></li>
+              <li><a>delete</a></li>
+            </ul>
+          </div>
+          {{-- <button class="btn">wishlist?</button> --}}
+          @error('required')
+          <x-alert-form>Error! {{ $message }}.</x-alert-form>
+          @enderror
           {{-- Header --}}
         </div>
-        <div class="overflow-x-auto">
+        <div class="overflow-x-auto" x-data="{ checked: false }">
           <table class="table">
             <thead>
               <tr>
                 <th>
                   <label>
-                    <input type="checkbox" class="checkbox" />
+                    <input type="checkbox" class="checkbox" x-model="checked"/>
                   </label>
                 </th>
                 <th>Image</th>
@@ -84,9 +99,12 @@
               <!-- row 1 -->
               <tr>
                 <th>
-                  <label>
-                    <input type="checkbox" class="checkbox" />
-                  </label>
+                    @csrf
+                    <label>
+                      <input type="checkbox" class="checkbox"  x-bind:checked="checked" name="check[]" value="{{ $product->id }}"/>
+                      <input type="hidden" value="{{ Auth::user()->id }}" name="user">
+                    </label>
+                  </form>
                 </th>
                 <td>
                   <div class="flex items-center gap-3">
