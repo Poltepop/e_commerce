20412bd2e9694a48edcfaf4e93b3b90beb3580f8
p4ddle <dewaputuadhitya@gmail.com>
2025-03-11 11:01:50 +0700

    Wishlist Route

--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,7 @@ use App\Http\Controllers\CategoryController;
 use App\Http\Controllers\HomeController;
 use App\Http\Controllers\ProductController;
 use App\Http\Controllers\UserController;
+use App\Http\Controllers\WishlistController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/',[HomeController::class, 'home']);
@@ -14,9 +15,6 @@ Route::get('/homepage',function(){
     ]);
 })->name('homepage');
 
-// Route::get('/posts',function(){
-//     return view('posts');
-// });
 
 Route::controller(ProductController::class)->middleware(['auth'])->group(function (){
     route::get('/products', 'product');
@@ -41,4 +39,9 @@ Route::controller(CategoryController::class)->middleware(['auth'])->group(functi
     route::post('/category/create', 'addCategory')->name('save.category');
     route::post('/category/{category:slug}/update', 'updateCategory')->name('update.category');
     route::post('/category/{id}/delete', 'deleteCategory')->name('delete.category');
+});
+
+Route::controller(WishlistController::class)->middleware(['auth'])->group(function(){
+    Route::get('/wishlist', 'wishlist');
+    Route::post('/wishlist/add', 'addWishlist')->name('add.wishlist');
 });
