a2d6a3610e4463a198d7b1b8ef9af7e8461bfa18
p4ddle <dewaputuadhitya@gmail.com>
2025-03-11 10:56:13 +0700

    Wishlist Controller

---
+++ app/Http/Controllers/WishlistController.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Product;
+use App\Models\User;
+use App\Services\WishlistService;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Validation\ValidationException;
+
+class WishlistController extends Controller
+{
+   private WishlistService $wishlistService;
+
+   public function __construct(WishlistService $wishlistService) {
+    $this->wishlistService = $wishlistService;
+   }
+
+    public function wishlist(){
+        $users = User::with('productsWishlists')->get();
+        return response()->view('admin.wishlist', [
+            'title' => 'Wishlist',
+            'users' => $users
+        ]);
+    }
+
+    public function addWishlist(Request $request){
+       try{
+        $wishlist = $request->validate([
+            'check' => 'required|array',
+            'user' => 'required',
+        ]);
+       }catch(ValidationException $e){
+        $firstError = $e->validator->errors()->first();
+        return back()->withErrors([
+            'required' => $firstError
+        ]);
+       }
+
+       $userid = $wishlist['user'];
+       unset($wishlist['user']);
+
+       $this->wishlistService->saveWishlist($wishlist['check'], $userid);
+       return redirect()->action([WishlistController::class, 'wishlist']);
+    }
+}
