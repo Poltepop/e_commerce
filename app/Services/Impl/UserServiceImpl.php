dcc889beaaa3bfa1eefce523e28f91b5a7471865
p4ddle <dewaputuadhitya@gmail.com>
2025-03-13 14:24:11 +0700

    User Register Service

--- app/Services/Impl/UserServiceImpl.php
+++ app/Services/Impl/UserServiceImpl.php
@@ -2,11 +2,16 @@
 
 namespace App\Services\Impl;
 
+use App\Models\User;
 use App\Services\UserService;
 use Illuminate\Support\Facades\Auth;
 
 class UserServiceImpl implements UserService {
     public function login(array $credentials){
         return Auth::attempt($credentials);
+    }
+
+    public function register(array $credentials){
+        return User::create($credentials);
     }
 }
--- app/Services/UserService.php
+++ app/Services/UserService.php
@@ -4,4 +4,6 @@ namespace App\Services;
 
 interface UserService {
     public function login(array $credentials);
+
+    public function register(array $credentials);
 }
